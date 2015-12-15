6274ec7a8c99715c4065ffd6434f9f0c6824cd36
libcrack <devnull@libcrack.so>
2015-12-15 02:05:45 +0100

    PEP8 gmongo.py

--- gmongo.py
+++ gmongo.py
@@ -1,7 +1,23 @@
-from gi.repository import Gtk
+import gi
+import sys
+import json
+import mongo
+import logging
+
+try:
+    gi.require_version("Gtk", "3.0")
+except Exception as e:
+    print("ERROR: GTK 3.0 required - {0}".format())
+    sys.exit(9)
 
 from views import MainWindow
+from gi.repository import Gtk
+from gi.repository import GdkPixbuf
 
+logname = 'gmongo'
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(logname)
+# logger.setLevel(logging.INFO)
 
 if __name__ == '__main__':
     window = MainWindow()
