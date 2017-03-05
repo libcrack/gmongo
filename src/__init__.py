88264a7f3f7fdb949c21195c36d8c92fddacb284
/dev/null <libcrack@users.noreply.github.com>
2017-03-05 20:07:02 +0100

    Mod: Updated git repo details

--- src/__init__.py
+++ src/__init__.py
@@ -5,5 +5,5 @@ gmongo
 __version__ = "0.1"
 __date__ = "mar dic 15 04:14:45 CET 2015"
 __author__ = "libcrack <devnull@libcrack.so>"
-__website__ = "https://github.com/borjiviri/telegrambot"
+__website__ = "https://github.com/libcrack/telegrambot"
 __license__ = "GNU General Public License version 3 or later"
